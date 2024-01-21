7b5bd640760a2a42f4a9c585822fe3a031aef508
unknown <shuhaibkymohamed@gmail.com>
2024-01-21 15:23:51 +0400

    updation

--- client/src/components/adminLogin.jsx
+++ client/src/components/adminLogin.jsx
@@ -9,7 +9,15 @@ function AdminLogin() {
     //validation
 const validate = values => {
     let errors = {}
-  
+  if(!values.name){
+    errors.email = toast.error('email is required')
+  }
+  else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+    errors.email = toast.error("invalid email address")
+}
+else if(!values.password){
+  errors.password = toast.error('password is required')
+}
     return errors
   }
   const formik = useFormik({
